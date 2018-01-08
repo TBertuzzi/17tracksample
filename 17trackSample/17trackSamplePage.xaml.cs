d3ea9613385d95d18e15e69374f730195792856b
Thiago Bertuzzi <bertuzzi@ness.com.br>
2018-01-08 14:49:43 -0200

    Primeira versão

---
+++ 17trackSample/17trackSamplePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace trackSample
+{
+    public partial class _7trackSamplePage : ContentPage
+    {
+        public _7trackSamplePage()
+        {
+            InitializeComponent();
+            webView.Source = LoadHTMLFileFromResource();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            webView.Eval(string.Format("doTrack('{0}')", txtRastrear.Text));
+        }
+
+
+        HtmlWebViewSource LoadHTMLFileFromResource()
+        {
+            var source = new HtmlWebViewSource();
+
+            var assembly = typeof(_7trackSamplePage).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("trackSample.Track.html");
+            using (var reader = new StreamReader(stream))
+            {
+                source.Html = reader.ReadToEnd();
+            }
+            return source;
+        }
+    }
+}
